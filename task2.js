acca66b5e492f236a086bd2c89d0a1cd8d49ebf0
ashraful2871 <ashrafulislam2758@gmail.com>
2024-08-29 08:14:56 +0600

    task2 complete

---
+++ task2.js
@@ -0,0 +1,18 @@
+//  Each Subject Number
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var bangla = 35.45;
+var english = 99.5;
+
+// calculate the total mark
+var totalMark = math + biology + chemistry + bangla + english;
+
+// Total subject
+var totalSub = 5;
+
+//Average mark
+var avg = totalMark / totalSub;
+
+//print result
+console.log(avg.toFixed(2));
