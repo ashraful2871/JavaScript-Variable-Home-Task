59573c53f602eb2c32d18d1624926b949d569885
ashraful2871 <ashrafulislam2758@gmail.com>
2024-08-29 08:01:59 +0600

    task1 complete

---
+++ task1.js
@@ -0,0 +1,6 @@
+var takaIHave = 1000; //Taka I Have
+var appleAndOrangePrice = 700; //1 kg Apple and Orange price
+
+var total = takaIHave - appleAndOrangePrice; //Declare 'total' with 'var'
+
+console.log(total); // output The Result
